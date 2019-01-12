2410794d74270dcec4874ffe2d1724caac78be43
Kaushik Pathak <sunils@exzeo.com>
2019-01-13 00:27:36 +0530

    added mongodb client and set render engine pug

--- index.js
+++ index.js
@@ -3,28 +3,40 @@
 const express = require('express');
 const config = require('config');
 const logger = require('winston');
-const mongoose = require('mongoose');
+
 const routes = require('./routes');
 const constants = require('./constants');
 const Middleware = require('./middleware');
 const apiHandler = require('./apiHandler');
 
+const DBManager = require('./lib/mongo-db-client').create(logger, config.config.db.mongoUrl);
 let app = express();
-mongoose.connect(config.config.db.mongoUrl, async (err) => {
-    if (err) {
-      return logger.error('Error in connecting to DB.', { errorData: err });
-    }
-    try {
-    const projects = await apiHandler(logger, constants.PROJECT_END_POINT);
-    const messages = await apiHandler(logger, constants.MESSAGE_END_POINT);
-    const contacts = await apiHandler(logger, constants.CONTACT_END_POINT);
-    const companies = await apiHandler(logger, constants.COMPANY_END_POINT);
-  } catch(error) {
-    console.log(error)
-  }
 
-    //insert projects, messages, contacts and companies in mongo db
-  });
+app.set('views', './views')
+app.set('view engine', 'pug');
+
+(async ()=>{
+  const dbConn = await DBManager.getConnection();
+  const {projects} = await apiHandler(logger, constants.PROJECT_END_POINT);
+  const {messages} = await apiHandler(logger, constants.MESSAGE_END_POINT);
+  const {contacts} = await apiHandler(logger, constants.CONTACT_END_POINT);
+  const {companies} = await apiHandler(logger, constants.COMPANY_END_POINT);
+
+  try {
+    // await dbConn.collection('projects').drop();
+    await dbConn.collection('projects').insertMany(projects);
+      // await dbConn.collection('messages').drop();
+    await dbConn.collection('messages').insertMany(messages);
+    // await dbConn.collection('contacts').drop();
+    await dbConn.collection('contacts').insertMany(contacts);
+    // await dbConn.collection('companies').drop();
+    await dbConn.collection('companies').insertMany(companies);
+    logger.info('database seeding to mongoDb completed: successfully');
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
 
 app.get('/health', (request, response) => {
     response.jsonp({
@@ -34,5 +46,8 @@ app.get('/health', (request, response) => {
 
 app.use(Middleware.errorHandler);
 app.use('/v1', routes);
+app.listen(config.config.common.port, function(){
+  logger.info(`Server started listening at port ${config.config.common.port}`)
+})
 
 module.exports = app;
