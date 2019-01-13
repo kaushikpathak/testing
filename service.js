5e6943f7c8c56f34dc50d7db1cf3ba556bf456de
Kaushik Pathak <sunils@exzeo.com>
2019-01-13 13:05:22 +0530

    added service layer for for pug data

--- service.js
+++ service.js
@@ -1,8 +1,43 @@
 
 function create(db){
     return {
-        async Projects(){
-            // db.
+        async getProjects(staffid){
+            staffid = parseInt(staffid); // using regex of better option
+
+            try {
+                const staff =  await db.collection('contacts').findOne({id:staffid});
+                let projectCompanies = await db.collection('projects').aggregate([
+                        {$match:{"staffid" : staffid}},
+                        {
+                            $lookup:{
+                                from: "companies",
+                                localField: "companyid",
+                                foreignField: "id",
+                                as: "company"
+                            }
+                        }
+                    ]).toArray();
+
+                projectCompanies  = projectCompanies.map(item=>{
+                    item.company = item.company[0];
+                    return item;
+                });
+
+                console.log(projectCompanies);  
+                
+                return {
+                    staff,
+                    projectCompanies
+                }
+
+            } catch (error) {
+                console.log('error', error);
+            }
+            
+            // await db.collection('').find();
+            // await db.collection('Projects').find();
         }
     }
 }
+
+module.exports = { create }
